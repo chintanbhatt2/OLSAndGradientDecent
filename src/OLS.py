17f36d1cce7f3604db49fa74f0a9c3041e34abfe
Chintan Bhatt <chintanbhatt2@gmail.com>
2021-10-21 17:13:03 -0700

    Calculates OLS model

--- src/OLS.py
+++ src/OLS.py
@@ -1,15 +1,24 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
 
 class OLS:
-    csv = None
+    csv1 = None
+    csv2 = None
     xAxis = None
+    xLabel = None
+    yLabel = None
     yAxis = None
+    fitModel = None
+    dataSet1 = []
+    dataSet2 = []
 
 
-    def readFile(self, path:str):
-        self.csv = pd.read_csv(path)
+    def readFile(self, path1:str, path2:str):
+        self.csv1 = pd.read_csv(path1, index_col=False)
+        self.csv2 = pd.read_csv(path2, index_col=False)
+        
     def __init__(self) -> None:
         pass
     def __eq__(self, o: object) -> bool:
@@ -17,5 +26,21 @@ class OLS:
 
 
     def setUpAxes(self, x:str, y:str):
-        self.xAxis = self.csv[x]
-        self.yAxis = self.csv[y]
+        self.xAxis = self.csv1[x]
+        self.xLabel = x
+        self.yAxis = self.csv2[y]
+        self.yLabel = y
+        
+
+    def classify(self):
+        model = sm.OLS(self.xAxis, self.yAxis);
+        self.fitModel = model.fit()
+        print(self.fitModel.params)
+        
+
+    def graph(self):
+        plt.scatter(x=self.xAxis, y=self.YAxis)
+        print(self.fitModel.predict())
+        plt.xlabel(self.xLabel)
+        plt.ylabel(self.yLabel)
+        plt.show()
--- src/main.py
+++ src/main.py
@@ -5,11 +5,13 @@ from LRGD import LRGD
 
 def main():
     ols = OLS()
-    ols.readFile("/home/cb/Desktop/Programming/Python/CS422/Assignment3/dataset/titanic.csv")
-    ols.setUpAxes("Fare", "Survived")
-    print(ols.csv)
-    print(ols.yAxis)
-    print(ols.xAxis)
+    ols.readFile("/home/cb/Desktop/Programming/Python/CS422/Assignment3/dataset/macroeconomics/formatted/CPI.csv", "/home/cb/Desktop/Programming/Python/CS422/Assignment3/dataset/macroeconomics/formatted/10YearInflation.csv")
+    print(ols.csv1)
+    print(ols.csv2)
+    ols.setUpAxes("CPI", "T10YIE")
+    ols.classify()
+    ols.graph()
+    
     
 
 if __name__ == "__main__":
